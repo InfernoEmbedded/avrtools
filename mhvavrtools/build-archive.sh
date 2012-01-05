6433ff1c6165c453520599759a9accaa992d00c4
Alastair D'Silva <deece@fedora15.(none)>
2012-01-06 03:26:16 +1100

    Fix archive names

--- mhvavrtools/build-archive.sh
+++ mhvavrtools/build-archive.sh
@@ -1,8 +1,8 @@
 date="`date +%Y%m%d`"
 
 if [ `uname -m` = 'i686' ]; then
-	7za a -m0=lzma -mx=9 -md=64m -sfx mhvavrtools-linux_x86-$date.7z mhvavrtools/
+	7za a -m0=lzma -mx=9 -md=64m -sfx mhvavrtools-linux_x86-$date mhvavrtools/
 else
-	7za a -m0=lzma -mx=9 -md=1024m -sfx mhvavrtools-linux_x86-$date.7z mhvavrtools/
+	7za a -m0=lzma -mx=9 -md=1024m -sfx mhvavrtools-linux_x64-$date mhvavrtools/
 fi
 
